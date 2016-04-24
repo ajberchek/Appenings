fbaed13eba886d18f39bd11779a8388b5ad2bbbc
ajberchek <ajberchek@gmail.com>
2016-04-23 20:22:45 -0700

    We now can download the json encoded data from the happenings site and we can parse out the data into its event json array

---
+++ app/src/main/java/com/example/aberchek/appenings/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.aberchek.appenings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DataPuller dp = new DataPuller();
+        dp.execute();
+
+        JSONObject jArr = dp.getJsonArr();
+        while((jArr = dp.getJsonArr()) == null)
+        {
+
+        }
+
+        Log.d("Server response",Integer.toString(jArr.length()));
+        try {
+            JSONObject eventList = jArr.getJSONObject("bwEventList");
+            Log.d("Event",eventList.toString());
+
+            JSONArray eventArr = eventList.getJSONArray("events");
+            Log.d("EventList",eventArr.toString());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
