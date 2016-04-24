9a26dd6fabd419b063b905f6d606302018d0d481
ajberchek <ajberchek@gmail.com>
2016-04-24 09:32:03 -0700

    Start working on link fixer

--- app/src/main/java/com/example/aberchek/appenings/DataPuller.java
+++ app/src/main/java/com/example/aberchek/appenings/DataPuller.java
@@ -48,63 +48,13 @@ public class DataPuller extends AsyncTask<String, Void, Boolean>
         return jsonData;
     }
 
-    public void grabData()
-    {
-        Log.d("haiiiiii","hai");
-        //String dataUrl = "http://events.ucmerced.edu:7070/feeder/main/eventsFeed.do";
-        String dataUrl = "http://events.ucmerced.edu:7070/feeder/main/eventsFeed.do?f=y&sort=dtstart.utc:asc&fexpr=(categories.href!=%22/public/.bedework/categories/sys/Ongoing%22)%20and%20(entity_type=%22event%22%7Centity_type=%22todo%22)&skinName=list-json&count=10";
-        String dataUrlParameters = "f=y&sort=dtstart.utc:asc&fexpr=(categories.href!=%22/public/.bedework/categories/sys/Ongoing%22)%20and%20(entity_type=%22event%22%7Centity_type=%22todo%22)&skinName=list-json";
-        URL url;
-        HttpURLConnection connection = null;
-        try {
-// Create connection
-            url = new URL(dataUrl);
-            connection = (HttpURLConnection) url.openConnection();
-            Log.d("haiiiiii","hai agn");
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
-            //connection.setRequestProperty("Content-Length","" + Integer.toString(dataUrlParameters.getBytes().length));
-            connection.setRequestProperty("Content-Language", "en-US");
-            connection.setUseCaches(false);
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-// Send request
-            //DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-            //wr.writeBytes(dataUrlParameters);
-            //wr.flush();
-           // wr.close();
-            Log.d("haiiiiii","hai blah blah");
-// Get Response
-            InputStream is = connection.getInputStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-            String line;
-            StringBuffer response = new StringBuffer();
-            while ((line = rd.readLine()) != null) {
-                response.append(line);
-                response.append('\r');
-            }
-            rd.close();
-            String responseStr = response.toString();
-            Log.d("Server response",responseStr);
-        } catch (Exception e) {
-
-            e.printStackTrace();
-
-        } finally {
-
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-
-    }
 
     public boolean tryGrabAgain()
     {
 
             try
             {
-                //Change the below url
+                //Change the below url if the site changes
                 String dataUrl = "http://events.ucmerced.edu:7070/feeder/main/eventsFeed.do?f=y&sort=dtstart.utc:asc&fexpr=(categories.href!=%22/public/.bedework/categories/sys/Ongoing%22)%20and%20(entity_type=%22event%22%7Centity_type=%22todo%22)&skinName=list-json&count=100&start=20160201&end=20160501";
 
                 URL url = new URL(dataUrl);
@@ -135,8 +85,6 @@ public class DataPuller extends AsyncTask<String, Void, Boolean>
             catch (JSONException e) {
                 e.printStackTrace();
             }
-        //   e.printStackTrace();
-           // }
 
         return false;
     }
