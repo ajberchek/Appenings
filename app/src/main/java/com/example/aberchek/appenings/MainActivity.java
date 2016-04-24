11e35d329de507280df6d3dec2a1dc3e96027ba9
Khrystarlite <ctaiine.rasengai@gmail.com>
2016-04-24 00:25:32 -0700

    scratch button work

--- app/src/main/java/com/example/aberchek/appenings/MainActivity.java
+++ app/src/main/java/com/example/aberchek/appenings/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.aberchek.appenings;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -11,7 +22,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<String> data =  new ArrayList<String>();
+    private ArrayList<happening> data =  new ArrayList<happening>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         DataPuller dp = new DataPuller();
         dp.execute();
+        fillData();
 
         JSONObject jArr = dp.getJsonArr();
         while((jArr = dp.getJsonArr()) == null)
@@ -37,8 +49,73 @@ public class MainActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        ListView lv = (ListView) findViewById(R.id.listView);
+
+        lv.setAdapter(new MyListAdaper(this, R.layout.list_item, data));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "List item was clicked at " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
+
+
+    private void fillData(){
+        for (int i = 0; i< 100; i++)
+            data.getTitle(i);
+
+    }
+    private class MyListAdaper extends ArrayAdapter<happening> {
+        private int layout;
+        private ArrayList<happening> mObjects;
+
+        private MyListAdaper(Context context, int resource, ArrayList<happening> objects) {
+            super(context, resource, objects);
+            mObjects = objects;
+            layout = resource;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder mainViewholder = null;
+            if (convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.title = (TextView) convertView.findViewById(R.id.list_item_text);
+                convertView.setTag(viewHolder);
+            }
+
+
+            return convertView;
+        }
+
+
+        public class ViewHolder {
+
+            ImageView thumbnail;
+            TextView title;
+            // Button button;
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 
 
